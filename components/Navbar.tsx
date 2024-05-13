9dd0c341bf7df358688beb2a7997999cca8cff1e
sdgsnehal <sdgsnehal@gmail.com>
2024-05-13 21:43:03 +0530

    style(Navbar.tsx): improve styling and layout of Navbar component for better readability and alignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,18 +7,18 @@ import { FaInstagramSquare } from "react-icons/fa";
 const Navbar = () => {
   return (
     <header className="w-full">
-      <nav className="flex  justify-evenly align-middle max-sm:flex-col">
+      <nav className="flex  justify-evenly max-sm:flex-col max-md:flex-col max-sm:items-center items-center">
         <Link href="/">
           <Image src="/assets/TopDog.png" alt="logo" width={180} height={100} />
         </Link>
-        <h1>Presented By</h1>
+        <h1 className="font-bold text-2xl">Presented By</h1>
         <Link href="/">
           <Image src="/assets/center.png" alt="logo" width={180} height={100} />
         </Link>
-        <h1>Fleet & Farm</h1>
-        <div>
-          <FaFacebook />
-          <FaInstagramSquare />
+        <h1 className="font-bold text-3xl">Fleet & Farm</h1>
+        <div className="flex justify-evenly max-sm:m-10">
+          <FaFacebook className="w-10 h-10" />
+          <FaInstagramSquare className="w-10 h-10" />
         </div>
       </nav>
     </header>
