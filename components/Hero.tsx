f96ebae02843a7386cfdd097060c21090afb6320
sdgsnehal <sdgsnehal@gmail.com>
2024-05-14 22:18:10 +0530

    style(Hero.tsx): update Hero component layout and styles for better visual presentation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>
-      <Image src="/assets/hero.webp" alt="hero" width={100} height={100}/>
+    <div className="bg-cover bg-center h-screen bg-[url('/assets/hero.webp')] relative">
+      <h1 className="absolute bottom-10 left-0 right-0 text-center font-extrabold text-5xl uppercase max-md:text-4xl max-sm:text-3xl">
+        Current Leaderboard
+      </h1>
     </div>
   );
 };
